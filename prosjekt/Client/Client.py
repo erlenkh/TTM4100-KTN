309dd52db115dccd211c7af51f55775a4d1210ac
simenbkr <simenbkr@users.noreply.github.com>
2016-02-29 21:29:52 +0100

    Update Client.py

--- prosjekt/Client/Client.py
+++ prosjekt/Client/Client.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-import socket
+import socket, json, re, time
 from MessageReceiver import MessageReceiver
 from MessageParser import MessageParser
+
+regex = re.compile(r'(.*) ?(.*)',re.M | re.I)
 
 class Client:
     """
@@ -13,28 +15,61 @@ class Client:
         This method is run when creating a new Client object
         """
 
+        self.host = host
+        self.server_port = server_port
         # Set up the socket connection to the server
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
+        self.kommandoer = ['login','logout','msg','names','help']
+
+        #self.mottaker = MessageReceiver(self,self.connection)
+
         # TODO: Finish init process with necessary code
         self.run()
 
     def run(self):
         # Initiate the connection to the server
         self.connection.connect((self.host, self.server_port))
+        print "[*] Kobling etablert"
+        lytter = MessageReciver(client,self.connection)
+        lytter.daemon = True
+        lytter.start()
+        print "[*] Lytter startet."
         
     def disconnect(self):
-        # TODO: Handle disconnection DONE
-        self.connection.close
+        self.connection.close()
+        # TODO: Handle disconnection
 
-    def receive_message(self, message):
-        # TODO: Handle incoming message
+    def receive_message(self, message, connection):
+        parser = MessageParser()
+        print parser.parse(message)
 
-
-    def send_payload(self, data):
+    def send_payload(self, kommandoen, melding):
         # TODO: Handle sending of a payload
-        pass
-        
+        payload = {}
+
+        payload['request'] = kommandoen
+        payload['content'] = melding
+        payload_json = json.dumps(payload)
+
+        self.connection.send(payload_json)
+
+
+    def handle_input(self):
+        a = True
+        while a:
+            inn_tekst = str(input("> "))
+            kommando = regex.search(inn.tekst).group(1)
+            if len(regex.search(inn_tekst).group().split(" ")) != 1 or len(regex.search(inn_tekst).group().split(" ")) != 2:
+                print "[*] Error: Ugyldig input."
+            elif kommando == 'logout':
+                self.disconnect()
+            elif kommando in kommandoer:
+                self.send_payload(kommando,regex.search(inn_tekst).group(2))
+                #a = False
+            else:
+                print "[*] Error: Ugyldig input 2"
+
     # More methods may be needed!
 
 
